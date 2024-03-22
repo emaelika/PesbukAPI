dc466771e5932232a0177c4089e88335b8fc5345
Abdul Malik <malik.the5th@gmail.com>
2024-03-22 14:16:06 +0700

    fixing routes

--- routes/routes.go
+++ routes/routes.go
@@ -15,21 +15,19 @@ func InitRoute(c *echo.Echo, ctl user.UserController, pc post.PostController, cc
 	c.POST("/login", ctl.Login())
 	c.GET("/users", ctl.Profile(), echojwt.WithConfig(echojwt.Config{
 		SigningKey: []byte(config.JWTSECRET),
-	})) 
-	c.PUT("/user", ctl.Update(), echojwt.WithConfig(echojwt.Config{
+	}))
+	c.PUT("/users", ctl.Update(), echojwt.WithConfig(echojwt.Config{
 		SigningKey: []byte(config.JWTSECRET),
 	}))
-	c.DELETE("/user", ctl.Delete(), echojwt.WithConfig(echojwt.Config{
+	c.DELETE("/users", ctl.Delete(), echojwt.WithConfig(echojwt.Config{
 		SigningKey: []byte(config.JWTSECRET),
 	}))
-	c.GET("/user/:id", ctl.GetUserByIDParam(), echojwt.WithConfig(echojwt.Config{
+	c.GET("/users/:id", ctl.GetUserByIDParam(), echojwt.WithConfig(echojwt.Config{
 		SigningKey: []byte(config.JWTSECRET),
 	}))
 	c.GET("/avatar", ctl.Avatar(), echojwt.WithConfig(echojwt.Config{
 		SigningKey: []byte(config.JWTSECRET),
 	}))
-
-
 
 	c.POST("/posts", pc.Add(), echojwt.WithConfig(echojwt.Config{
 		SigningKey: []byte(config.JWTSECRET),
@@ -44,11 +42,6 @@ func InitRoute(c *echo.Echo, ctl user.UserController, pc post.PostController, cc
 	c.DELETE("/posts/:id", pc.Delete(), echojwt.WithConfig(echojwt.Config{
 		SigningKey: []byte(config.JWTSECRET),
 	}))
-
-
-
-
-
 
 	c.POST("/comments", cc.Add(), echojwt.WithConfig(echojwt.Config{
 		SigningKey: []byte(config.JWTSECRET),
